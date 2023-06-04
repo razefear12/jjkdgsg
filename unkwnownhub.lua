c24e99ceb91830f031c5c2e5f348294f9454bbf0
razefear12 <128930496+razefear12@users.noreply.github.com>
2023-06-04 21:28:32 +0300

    Update unkwnownhub.lua

--- unkwnownhub.lua
+++ unkwnownhub.lua
@@ -9,33 +9,6 @@ tab:Button("Button", function()
     lib:Notification("Notification", "Hello!", "Hi!")
 end)
 
-/*
-tab:Toggle("Toggle", false, function(t)
-    print(t)
-end)
-
-tab:Slider("Slider", 0, 100, 30, function(t)
-    print(t)
-end)
-
-tab:Dropdown("Dropdown", {"Option 1", "Option 2", "Option 3", "Option 4", "Option 5"}, function(t)
-    print(t)
-end)
-
-tab:Colorpicker("Colorpicker", Color3.fromRGB(255, 0, 0), function(t)
-    print(t)
-end)
-
-tab:Textbox("Textbox", true, function(t)
-    print(t)
-end)
-
-tab:Bind("Bind", Enum.KeyCode.RightShift, function()
-    print("Pressed!")
-end)
-*/
-
-
 
 local spins = win:Tab("Spins")
 

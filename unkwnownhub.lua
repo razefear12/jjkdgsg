74cbb0a8f255e16cd620e23a0810245476e36fe6
razefear12 <128930496+razefear12@users.noreply.github.com>
2023-06-04 20:47:06 +0300

    Update unkwnownhub.lua

--- unkwnownhub.lua
+++ unkwnownhub.lua
@@ -1,19 +1,5 @@
 local lib = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/Vape.txt")()
 
-local clans = {"Uzui", "Kamado", "Rengoku", "Agatsuma"};
-
-local players = game:GetService("Players");
-local runService = game:GetService("RunService");
-local replicatedStorage = game:GetService("ReplicatedStorage");
-
-local localPlayer = players.LocalPlayer;
-local playerData = replicatedStorage:WaitForChild("Player_Data");
-local localData = playerData:WaitForChild(localPlayer.Name);
-local clan = localData:WaitForChild("Clan");
-
-local remotes = replicatedStorage:WaitForChild("Remotes");
-local sendServer = remotes:WaitForChild("To_Server");
-local handleSpin = sendServer:WaitForChild("Handle_Initiate_S_");
 
 local win = lib:Window("PREVIEW", Color3.fromRGB(44, 120, 224), Enum.KeyCode.RightControl)
 
@@ -51,15 +37,6 @@ tab:Label("Label")
 
 -- Обработчик события для выполнения Lua-скрипта
 tab:Button("Execute Script", function()
-    while task.wait() do
-    if (not table.find(clans, clan.Value)) then
-       if (game.PlaceId ~= 5956785391) then
-           localPlayer:Kick("use the script in the main menu");
-       else
-           handleSpin:InvokeServer("check_can_spin");
-       end
-   end
-end
 end)
 
 local changeclr = win:Tab("Change UI Color")

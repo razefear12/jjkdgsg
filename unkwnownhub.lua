9027c230df3aad4fcce76315c0575f84da57bf1c
razefear12 <128930496+razefear12@users.noreply.github.com>
2023-06-04 21:33:23 +0300

    Update unkwnownhub.lua

--- unkwnownhub.lua
+++ unkwnownhub.lua
@@ -9,18 +9,6 @@ tab:Button("Button", function()
     lib:Notification("Notification", "Hello!", "Hi!")
 end)
 
-local lib = loadstring(game:HttpGet"https://raw.githubusercontent.com/razefear12/jjkdgsg/main/ui.lua")()
-
-
-local win = lib:Window("UnknownHub | Project Slayers", Color3.fromRGB(140, 44, 224), Enum.KeyCode.RightControl)
-
-local tab = win:Tab("Main")
-
-tab:Button("Button", function()
-    lib:Notification("Notification", "Hello!", "Hi!")
-end)
-
-
 tab:Toggle("Toggle", false, function(t)
     print(t)
 end)
@@ -41,10 +29,6 @@ tab:Textbox("Textbox", true, function(t)
     print(t)
 end)
 
-
-
-
-
 local spins = win:Tab("Spins")
 
 -- Обработчик события для выполнения Lua-скрипта
@@ -57,28 +41,6 @@ spins:Button("Daily auto-spin", function()
         end
     end    
 end)
-
-
-local changeclr = win:Tab("Change UI Color")
-
-changeclr:Colorpicker("Change UI Color", Color3.fromRGB(140, 44, 224), function(t)
-    lib:ChangePresetColor(Color3.fromRGB(t.R * 255, t.G * 255, t.B * 255))
-end)
-
-
-local spins = win:Tab("Spins")
-
--- Обработчик события для выполнения Lua-скрипта
-spins:Button("Daily auto-spin", function(t)
-    while true do
-        wait()
-        if game.PlaceId ~= 5956785391 then
-            lib:Notification("Daily auto-spin", "", "OK")
-            game:GetService("ReplicatedStorage"):WaitForChild("spins_thing_remote"):InvokeServer()
-        end
-    end    
-end)
-
 
 local changeclr = win:Tab("Change UI Color")
 

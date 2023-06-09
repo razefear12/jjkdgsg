490e5c95edfca65d0a7f49fc396baf029b4484fc
razefear12 <128930496+razefear12@users.noreply.github.com>
2023-06-09 17:38:06 +0300

    Update unknownloader.lua

--- unknownloader.lua
+++ unknownloader.lua
@@ -15,7 +15,5 @@ elseif placeId == 11468159863 then
     print("You are in a different Place ID")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/razefear12/jjkdgsg/main/unknownMap2.lua", true))()
 else
-   -- Код для всех остальных Place ID
    print("Nice! <3")
-   -- Добавьте свой код здесь
 end

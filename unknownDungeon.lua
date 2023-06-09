2b678a96daa439d92446b7d121a59dbaa2940816
razefear12 <128930496+razefear12@users.noreply.github.com>
2023-06-09 17:31:44 +0300

    Update unknownDungeon.lua

--- unknownDungeon.lua
+++ unknownDungeon.lua
@@ -105,15 +105,145 @@ maintab:Button("Farm | FPS-BOOST", function()
     loadstring(game:HttpGet('https://raw.githubusercontent.com/MarsQQ/ScriptHubScripts/master/FPS%20Boost', true))()
 end)
 
+maintab:Toggle("Auto Loot Chest", false, function(t)
+    isToggleMobsTweenEnabled = t -- Обновляем состояние переключателя
+    if t then
+
+    end      
+end)
+
+local misc = win:Tab("Misc")
+
+local kamadonhealon = true
+local kamadonhealoff = false
+
+misc:Toggle("Kamado Heal (Only Demon)", false, function(t)
+    kamadonhealon = t
+    if not t then
+        kamadonhealon = false
+        kamadonhealoff = true
+    end
+    game:GetService("ReplicatedStorage").Remotes.heal_tang123asd:FireServer(kamadonhealon)
+end)
+
+
+local wardrumsbuffon = true
+local wardrumsbuffoff = false
+
+misc:Toggle("Dmg Buff (All Race)", false, function(t)
+
+    wardrumsbuffon = t
+    if not t then
+        wardrumsbuffon = false
+        wardrumsbuffoff = true
+    end
+    while wardrumsbuffon do
+    game:GetService("ReplicatedStorage").Remotes.war_Drums_remote:FireServer(wardrumsbuffon)
+    wait(2)
+    end
+end)
+
+
+local godmodeakazabda1 = "skil_ting_asd"
+local godmodeakazabda2 = game:GetService("Players").LocalPlayer
+local godmodeakazabda3 = "akaza_bda_compass_needle"
+local godmodeakazabda4 = 1
+local isGodModeEnabled = false
+local godModeLoop
+
+local function startGodMode()
+    if isGodModeEnabled then
+        game:GetService("ReplicatedStorage").Remotes.To_Server.Handle_Initiate_S:FireServer(godmodeakazabda1, godmodeakazabda2, godmodeakazabda3, godmodeakazabda4)
+        wait(1.5)
+        startGodMode()
+    end
+end
+
+misc:Toggle("Akaza BDA (God Mode)", false, function(t)
+    isGodModeEnabled = t
+    if isGodModeEnabled then
+        startGodMode()
+    elseif godModeLoop then
+        godModeLoop:Disconnect()
+    end
+end)
+
+
+
+local BuffsSelector = {"Exp+Chance", "Polar", "Devourer", "Damage", "Champion"} -- Обновленный список баффов
+local selectedOption = BuffsSelector[1] -- Значение по умолчанию
+local expchancebuffon = false -- Переменная для отслеживания состояния переключателя
+local buffs -- Переменная для хранения выбранного баффа
+
+misc:Dropdown("Select Buffs:", BuffsSelector, function(option)
+    selectedOption = option
+end)
+
+misc:Toggle("Farm Buffs", false, function(t)
+    expchancebuffon = t -- Обновляем состояние переключателя
+    if t then
+        if selectedOption == "Exp+Chance" then
+            buffs = "Yatagarasu Mask"
+        elseif selectedOption == "Polar" then
+            buffs = "Polar Top"
+        elseif selectedOption == "Devourer" then
+            buffs = "Devourer Top"
+        elseif selectedOption == "Damage" then
+            buffs = "Rengoku Haorie"
+        elseif selectedOption == "Champion" then
+            buffs = "Champion Haorie"
+        end
+
+        for i, v in pairs(game.ReplicatedStorage.Player_Data[game.Players.LocalPlayer.Name].EquippedItemStats:GetChildren()) do
+            if v:IsA("StringValue") then
+                v.Value = buffs
+            end
+        end
+    else
+        for i, v in pairs(game.ReplicatedStorage.Player_Data[game.Players.LocalPlayer.Name].EquippedItemStats:GetChildren()) do
+            if v:IsA("StringValue") then
+                v.Value = "" -- Отключаем баффы, устанавливая пустое значение
+            end
+        end
+    end
+end)
+
+
+
+
+misc:Button("Anti SunDamage (Only Demon)", function()
+    game:GetService("Players").LocalPlayer.PlayerScripts.Small_Scripts.Gameplay.Sun_Damage.Disabled = true
+end)
+
+misc:Button("Inf Stamina", function()
+    game:GetService("Players").LocalPlayer.PlayerScripts.Small_Scripts.Gameplay.Stamina.Disabled = true
+    wait(0.5)
+    game:GetService("Players").LocalPlayer.PlayerScripts.Small_Scripts.Gameplay.Stamina.Disabled = false
+    wait(0.5)
+    game:GetService("Players").LocalPlayer.PlayerScripts.Small_Scripts.Gameplay.Stamina.Disabled = true
+end)
+
+misc:Button("Inf Breathing", function()
+    game:GetService("Players").LocalPlayer.PlayerScripts.Small_Scripts.Gameplay.Breathing.Disabled = true
+    wait(0.5)
+    game:GetService("Players").LocalPlayer.PlayerScripts.Small_Scripts.Gameplay.Breathing.Disabled = false
+    wait(0.5)
+    game:GetService("Players").LocalPlayer.PlayerScripts.Small_Scripts.Gameplay.Breathing.Disabled = true
+end)
+
+
+local killaura = win:Tab("Killaura")
+
+
 local WeaponSelector = {"Combat", "Sword", "Claw", "Scythe", "Fans"} -- Обновленный список оружия
 local selectedOption = WeaponSelector[1] -- Значение по умолчанию
 local isToggleEnabled = false -- Переменная для отслеживания состояния переключателя
 
-maintab:Dropdown("Select Weapon:", WeaponSelector, function(option)
+killaura:Dropdown("Select Weapon:", WeaponSelector, function(option)
     selectedOption = option
 end)
 
-maintab:Toggle("Killaura V1", false, function(t)
+killaura:Toggle("Killaura V1", false, function(t)
     isToggleEnabled = t -- Обновляем состояние переключателя
     if t then
         local weapon
@@ -191,7 +321,7 @@ maintab:Toggle("Killaura V1", false, function(t)
     end
 end)
 
-maintab:Toggle("Killaura V2", false, function(t)
+killaura:Toggle("Killaura V2", false, function(t)
     isToggleEnabled = t -- Обновляем состояние переключателя
     if t then
      local weapon
@@ -384,7 +514,7 @@ end
 end)
 
 
-maintab:Toggle("Killaura (JUMP)", false, function(t)
+killaura:Toggle("Killaura (JUMP)", false, function(t)
     isToggleEnabled = t -- Обновляем состояние переключателя
     if t then
      local weapon
@@ -470,130 +600,4 @@ maintab:Toggle("Killaura (JUMP)", false, function(t)
         wait(1.3) -- Добавляем задержку в 1 секунду перед следующей итерацией цикла
     end
 end
-end)
-
-maintab:Toggle("Auto Loot Chest", false, function(t)
-    isToggleMobsTweenEnabled = t -- Обновляем состояние переключателя
-    if t then
-
-    end      
-end)
-
-local misc = win:Tab("Misc")
-
-local kamadonhealon = true
-local kamadonhealoff = false
-
-misc:Toggle("Kamado Heal (Only Demon)", false, function(t)
-    kamadonhealon = t
-    if not t then
-        kamadonhealon = false
-        kamadonhealoff = true
-    end
-    game:GetService("ReplicatedStorage").Remotes.heal_tang123asd:FireServer(kamadonhealon)
-end)
-
-
-local wardrumsbuffon = true
-local wardrumsbuffoff = false
-
-misc:Toggle("Dmg Buff (All Race)", false, function(t)
-
-    wardrumsbuffon = t
-    if not t then
-        wardrumsbuffon = false
-        wardrumsbuffoff = true
-    end
-    while wardrumsbuffon do
-    game:GetService("ReplicatedStorage").Remotes.war_Drums_remote:FireServer(wardrumsbuffon)
-    wait(2)
-    end
-end)
-
-
-local godmodeakazabda1 = "skil_ting_asd"
-local godmodeakazabda2 = game:GetService("Players").LocalPlayer
-local godmodeakazabda3 = "akaza_bda_compass_needle"
-local godmodeakazabda4 = 1
-local isGodModeEnabled = false
-local godModeLoop
-
-local function startGodMode()
-    if isGodModeEnabled then
-        game:GetService("ReplicatedStorage").Remotes.To_Server.Handle_Initiate_S:FireServer(godmodeakazabda1, godmodeakazabda2, godmodeakazabda3, godmodeakazabda4)
-        wait(1.5)
-        startGodMode()
-    end
-end
-
-misc:Toggle("Akaza BDA (God Mode)", false, function(t)
-    isGodModeEnabled = t
-    if isGodModeEnabled then
-        startGodMode()
-    elseif godModeLoop then
-        godModeLoop:Disconnect()
-    end
-end)
-
-
-
-local BuffsSelector = {"Exp+Chance", "Polar", "Devourer", "Damage", "Champion"} -- Обновленный список баффов
-local selectedOption = BuffsSelector[1] -- Значение по умолчанию
-local expchancebuffon = false -- Переменная для отслеживания состояния переключателя
-local buffs -- Переменная для хранения выбранного баффа
-
-misc:Dropdown("Select Buffs:", BuffsSelector, function(option)
-    selectedOption = option
-end)
-
-misc:Toggle("Farm Buffs", false, function(t)
-    expchancebuffon = t -- Обновляем состояние переключателя
-    if t then
-        if selectedOption == "Exp+Chance" then
-            buffs = "Yatagarasu Mask"
-        elseif selectedOption == "Polar" then
-            buffs = "Polar Top"
-        elseif selectedOption == "Devourer" then
-            buffs = "Devourer Top"
-        elseif selectedOption == "Damage" then
-            buffs = "Rengoku Haorie"
-        elseif selectedOption == "Champion" then
-            buffs = "Champion Haorie"
-        end
-
-        for i, v in pairs(game.ReplicatedStorage.Player_Data[game.Players.LocalPlayer.Name].EquippedItemStats:GetChildren()) do
-            if v:IsA("StringValue") then
-                v.Value = buffs
-            end
-        end
-    else
-        for i, v in pairs(game.ReplicatedStorage.Player_Data[game.Players.LocalPlayer.Name].EquippedItemStats:GetChildren()) do
-            if v:IsA("StringValue") then
-                v.Value = "" -- Отключаем баффы, устанавливая пустое значение
-            end
-        end
-    end
-end)
-
-
-
-
-misc:Button("Anti SunDamage (Only Demon)", function()
-    game:GetService("Players").LocalPlayer.PlayerScripts.Small_Scripts.Gameplay.Sun_Damage.Disabled = true
-end)
-
-misc:Button("Inf Stamina", function()
-    game:GetService("Players").LocalPlayer.PlayerScripts.Small_Scripts.Gameplay.Stamina.Disabled = true
-    wait(0.5)
-    game:GetService("Players").LocalPlayer.PlayerScripts.Small_Scripts.Gameplay.Stamina.Disabled = false
-    wait(0.5)
-    game:GetService("Players").LocalPlayer.PlayerScripts.Small_Scripts.Gameplay.Stamina.Disabled = true
-end)
-
-misc:Button("Inf Breathing", function()
-    game:GetService("Players").LocalPlayer.PlayerScripts.Small_Scripts.Gameplay.Breathing.Disabled = true
-    wait(0.5)
-    game:GetService("Players").LocalPlayer.PlayerScripts.Small_Scripts.Gameplay.Breathing.Disabled = false
-    wait(0.5)
-    game:GetService("Players").LocalPlayer.PlayerScripts.Small_Scripts.Gameplay.Breathing.Disabled = true
 end)

2121b1a5b8d6c94403901c8c81d1c8b80684d215
razefear12 <128930496+razefear12@users.noreply.github.com>
2023-06-09 17:40:07 +0300

    Update unknownloader.lua

--- unknownloader.lua
+++ unknownloader.lua
@@ -3,17 +3,17 @@ local placeId = game.PlaceId
 print("Script loaded...")
 
 if placeId == 5956785391 then
-    print("You are in a different Place ID")
+    print("Nice! <3")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/razefear12/jjkdgsg/main/unknownMainmenu.lua", true))()
 elseif placeId == 11468075017 then
-    print("You are in a different Place ID")
+    print("Nice! <3")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/razefear12/jjkdgsg/main/unknownDungeon.lua", true))()
 elseif placeId == 6152116144 then
-    print("You are in a different Place ID")
+    print("Nice! <3")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/razefear12/jjkdgsg/main/unknownMap1.lua", true))()
 elseif placeId == 11468159863 then
-    print("You are in a different Place ID")
+    print("Nice! <3")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/razefear12/jjkdgsg/main/unknownMap2.lua", true))()
 else
-   print("Nice! <3")
+   print("Script is temporarily not working in this Place ID.")
 end
